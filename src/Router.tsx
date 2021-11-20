49bb3f7b96263ff7c24de156424e0f40a07f597d
Vadim <vadim.putrov@gmail.com>
2021-11-20 12:27:58 +0200

    rename from Home to VideoStreaming

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, Link, Redirect } from "react-router-dom";
-import Home from "pages/Home/Home";
+import VideoStreaming from "pages/VideoStreaming/VideoStreaming";
 import Settings from "pages/Settings/Settings";
 import MediaFiles from "./pages/MediaFiles/MediaFiles";
 import { routes } from "routes";
@@ -8,7 +8,7 @@ import { routes } from "routes";
 const Router = () => {
   return (
     <Switch>
-      <Route exact path={routes.videoStreaming} component={Home} />
+      <Route exact path={routes.videoStreaming} component={VideoStreaming} />
       <Route exact path={routes.mediaFiles} component={MediaFiles} />
       <Route exact path={routes.settings} component={Settings} />
 
