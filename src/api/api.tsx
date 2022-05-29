7e37da73722b33d405d999ecfc5f9bd7a04f580a
Vadim <vadim.putrov@gmail.com>
2022-05-29 11:20:36 +0300

    change api url

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-// export const API_URL = `${document.location.protocol}"//"${document.location.hostname}:8080`;
-export const API_URL = "http://raspberrypi.local:8080";
-
-// eslint-disable-next-line no-console
-console.log(process.env.NODE_ENV);
+export const API_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://raspberrypi.local:8080"
+    : `${document.location.protocol}"//"${document.location.hostname}:8080`;
 
 export const getUserId = () => axios.get(`${API_URL}/get_user_id`);
