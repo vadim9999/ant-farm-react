0826285baebda7e762923dcc860d88931b76f5a1
Vadim <vadim.putrov@gmail.com>
2022-06-04 15:28:03 +0300

    fix api url

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -3,6 +3,6 @@ import axios from "axios";
 export const API_URL =
   process.env.NODE_ENV === "development"
     ? "http://raspberrypi.local:8080"
-    : `${document.location.protocol}"//"${document.location.hostname}:8080`;
+    : `${document.location.protocol}//${document.location.hostname}:8080`;
 
 export const getUserId = () => axios.get(`${API_URL}/get_user_id`);
